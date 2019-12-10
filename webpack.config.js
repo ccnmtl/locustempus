249564bb008f95b5e3b58b2b3e788f7bd1362213
Nick Buonincontri <nbuonin@gmail.com>
2019-12-10 12:11:13 -0500

    Implement Webpack setup
    This commit configures Webpack. It makes some changes to the CTL's
    typical media setup. It removes `media/js` and `media/css` folders in
    favor of a single media/src folder. Compiled assets will be saved to
    `media/build`.
    
    To run the build, use `make webpack`.
    
    This commit also removes require.js because Webpack is now being used to
    manage js dependencies.

---
+++ webpack.config.js
@@ -0,0 +1,74 @@
+/* eslint-disable */
+const path = require('path');
+const webpack = require('webpack');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
+module.exports = {
+    entry: {
+        main: './media/src/main.jsx',
+    },
+    mode: process.env.WEBPACK_SERVE ? 'development' : 'production',
+    output: {
+        path: path.resolve(__dirname, 'media/build'),
+        filename: 'bundle.js'
+    },
+    resolve: {
+        extensions: ['*', '.js', '.jsx']
+    },
+    module: {
+        rules: [
+            {
+                test: /\.(js|jsx)$/,
+                include: path.resolve(__dirname, 'media/src'),
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['@babel/preset-env', '@babel/preset-react']
+                    }
+                }
+            },
+            {
+                test: /\.(scss)$/,
+                use: [
+                    {
+                        loader: MiniCssExtractPlugin.loader,
+                        options: {
+                            sourceMap: true,
+                        }
+                    },
+                    {
+                        loader: 'css-loader',
+                        options: {
+                            sourceMap: true,
+                        }
+                    },
+                    {
+                        loader: 'postcss-loader',
+                        options: {
+                            plugins: function() {
+                                return [
+                                    require('precss'),
+                                    require('autoprefixer')
+                                ]
+                            },
+                            sourceMap: true,
+                        }
+                    },
+                    {
+                        loader: 'sass-loader',
+                        options: {
+                            sourceMap: true,
+                        }
+                    }
+                ]
+            },
+        ],
+    },
+    plugins: [
+        new MiniCssExtractPlugin({
+            filename: '[name].css',
+            chunkFilename: '[id].css',
+        }),
+    ]
+};
