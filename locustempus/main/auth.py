9bc7e0c1ae2488001c18b59c6a26827c849914ef
Nick Buonincontri <nbuonin@gmail.com>
2020-01-29 12:45:34 -0500

    Implement Course mapping
    This commit copies over course mapping functionality from Mediathread.

---
+++ locustempus/main/auth.py
@@ -0,0 +1,69 @@
+from django.conf import settings
+from django.contrib.auth.models import Group
+from django.utils import timezone
+from courseaffils.models import Affil
+
+
+class CourseGroupMapper(object):
+    """
+    See if the user is part of any existing course. If so, add them
+    as a member.
+    """
+
+    @staticmethod
+    def create_activatable_affil(user, affil, year) -> None:
+        """Create an Affil for the affil/user.
+
+        The required conditions are:
+        - The user has 'faculty' status for this affil
+        - The affil has a recent or future date.
+
+        Returns the Affil if created, otherwise returns None.
+        """
+        if hasattr(settings, 'COURSEAFFILS_COURSESTRING_MAPPER'):
+            d = settings.COURSEAFFILS_COURSESTRING_MAPPER.to_dict(affil)
+        else:
+            return None
+
+        try:
+            conditions = user is not None and \
+                         user.is_authenticated and \
+                         d is not None and \
+                         d['member'] == 'fc' and \
+                         year is not None and \
+                         year <= int(d['year'])
+        except KeyError:
+            return None
+
+        if conditions:
+            return Affil.objects.get_or_create(
+                name=affil, user=user)[0]
+
+        return None
+
+    @staticmethod
+    def map(user, affils):
+        # we also make a "pseudo" affil group ALL_CU
+        # that contains *anyone* who's logged in through WIND
+        affils.append("ALL_CU")
+
+        # by default, WIND affils include a group named for
+        # the uni for each user. This is not usually desirable
+        # so we strip it out, but there's a setting that lets
+        # you turn it back on.
+        remove_uni = True
+        if hasattr(settings, 'WIND_AFFIL_GROUP_INCLUDE_UNI_GROUP'):
+            if settings.WIND_AFFIL_GROUP_INCLUDE_UNI_GROUP is True:
+                remove_uni = False
+
+        year = timezone.now().year
+
+        for affil in affils:
+            if remove_uni and (affil == user.username):
+                continue
+            try:
+                group = Group.objects.get(name=affil)
+                user.groups.add(group)
+                user.save()
+            except Group.DoesNotExist:
+                CourseGroupMapper.create_activatable_affil(user, affil, year)
---
+++ locustempus/main/tests/test_auth.py
@@ -0,0 +1,93 @@
+from django.test import TestCase, override_settings
+from django.contrib.auth.models import AnonymousUser
+from courseaffils.columbia import CourseStringMapper
+
+from locustempus.main.tests.factories import UserFactory, CourseFactory
+from locustempus.main.auth import CourseGroupMapper
+
+
+@override_settings(COURSEAFFILS_COURSESTRING_MAPPER=CourseStringMapper)
+class TestCourseGroupMapper(TestCase):
+    def setUp(self):
+        self.user = UserFactory.create()
+        self.course = CourseFactory.create()
+
+    def test_map_user_uniremoved(self):
+        affils = [self.user.username]
+        self.assertEqual(self.user.groups.count(), 0)
+
+        CourseGroupMapper.map(self.user, affils)
+        self.assertEqual(self.user.groups.count(), 0)
+
+    def test_map_user_group_does_not_exist(self):
+        affils = [self.user.username,
+                  'CUcourse_ENGLW3872:columbia.edu']
+        self.assertEqual(self.user.groups.count(), 0)
+
+        CourseGroupMapper.map(self.user, affils)
+        self.assertEqual(self.user.groups.count(), 0)
+
+    def test_map_user_student_affiliation(self):
+        affils = [self.user.username, self.course.group.name]
+        self.assertEqual(self.user.groups.count(), 0)
+
+        CourseGroupMapper.map(self.user, affils)
+        self.assertEqual(self.user.groups.count(), 1)
+        self.assertTrue(self.course.group in self.user.groups.all())
+
+    def test_map_user_faculty_affiliation(self):
+        affils = [self.user.username,
+                  self.course.group.name,
+                  self.course.faculty_group.name]
+
+        self.assertEqual(self.user.groups.count(), 0)
+
+        CourseGroupMapper.map(self.user, affils)
+        self.assertEqual(self.user.groups.count(), 2)
+        self.assertTrue(self.course.group in self.user.groups.all())
+        self.assertTrue(self.course.faculty_group in self.user.groups.all())
+
+    def test_create_activatable_affil_empty_course_string(self):
+        self.assertIsNone(
+            CourseGroupMapper.create_activatable_affil(self.user, '', 2016))
+        self.assertIsNone(
+            CourseGroupMapper.create_activatable_affil(self.user, '', None))
+        self.assertIsNone(
+            CourseGroupMapper.create_activatable_affil(None, '', None))
+        self.assertIsNone(
+            CourseGroupMapper.create_activatable_affil(
+                AnonymousUser(), '', 2016))
+
+    def test_create_activatable_affil_student_course_string(self):
+        s = 't1.y2016.s001.cf1000.scnc.st.course:columbia.edu'
+        self.assertIsNone(
+            CourseGroupMapper.create_activatable_affil(self.user, s, 2016))
+        self.assertIsNone(
+            CourseGroupMapper.create_activatable_affil(self.user, s, None))
+        self.assertIsNone(
+            CourseGroupMapper.create_activatable_affil(None, s, None))
+        self.assertIsNone(
+            CourseGroupMapper.create_activatable_affil(
+                AnonymousUser(), s, 2016))
+
+    def test_create_activatable_affil_faculty_course_string(self):
+        s = 't1.y2016.s001.cf1000.scnc.fc.course:columbia.edu'
+        aa = CourseGroupMapper.create_activatable_affil(self.user, s, 2016)
+        self.assertIsNotNone(aa)
+        self.assertEqual(aa.name, s)
+        self.assertEqual(aa.user, self.user)
+
+        self.assertIsNone(
+            CourseGroupMapper.create_activatable_affil(self.user, s, 2017))
+        self.assertIsNone(
+            CourseGroupMapper.create_activatable_affil(self.user, s, None))
+        self.assertIsNone(
+            CourseGroupMapper.create_activatable_affil(None, s, None))
+        self.assertIsNone(
+            CourseGroupMapper.create_activatable_affil(
+                AnonymousUser(), s, 2016))
+
+    def test_create_activatable_affil_misc_course_string(self):
+        self.assertIsNone(
+            CourseGroupMapper.create_activatable_affil(
+                self.user, 'ALL_CU', 2016))
