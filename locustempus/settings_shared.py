acffd6ac9a90ae108e5402c15a22b21e7c3c0ee0
Nick Buonincontri <nbuonin@gmail.com>
2020-02-26 16:15:01 -0500

    Add needed Django conf for front-end testing

--- locustempus/settings_shared.py
+++ locustempus/settings_shared.py
@@ -67,7 +67,9 @@ if 'integrationserver' in sys.argv:
             'ATOMIC_REQUESTS': True,
         }
     }
-
+    EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+    SESSION_COOKIE_SECURE = False
+    CSRF_COOKIE_SECURE = False
     PASSWORD_HASHERS = (
         'django.contrib.auth.hashers.MD5PasswordHasher',
     )
