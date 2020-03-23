1595dff01b5e2a48b59a2426b1bca898def33f6a
Nick Buonincontri <nbuonin@gmail.com>
2020-03-23 14:17:11 -0400

    Implements Layer generic foreign key

--- locustempus/main/models.py
+++ locustempus/main/models.py
@@ -1,6 +1,18 @@
 """Models for the Locus Tempus application"""
 from courseaffils.models import Course
+from django.contrib.auth.models import User
+from django.contrib.contenttypes.fields import (
+    GenericForeignKey, GenericRelation
+)
+from django.contrib.contenttypes.models import ContentType
 from django.db import models
+
+
+class Layer(models.Model):
+    title = models.CharField(max_length=32)
+    content_type = models.ForeignKey(ContentType, on_delete=models.CASCADE)
+    object_id = models.PositiveIntegerField()
+    content_object = GenericForeignKey('content_type', 'object_id')
 
 
 class Project(models.Model):
@@ -24,6 +36,21 @@ class Project(models.Model):
         choices=BASE_MAPS,
         default='dark-v10'
     )
+    layers = GenericRelation(Layer, related_query_name='project')
 
     def __str__(self):
         return self.title
+
+
+class Response(models.Model):
+    project = models.ForeignKey(
+        Project,
+        on_delete=models.CASCADE,
+        related_name='responses'
+    )
+    user = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        related_name='+'
+    )
+    layers = GenericRelation(Layer, related_query_name='layer')
