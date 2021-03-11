b1ad2f3fc22c0f440d5fe04326745e8f7da9a6a4
Zarina Mustapha <zarina@columbia.edu>
2021-03-11 13:22:34 -0500

    Changed variable names in views.py and other places in templates

--- locustempus/main/tests/test_views_course.py
+++ locustempus/main/tests/test_views_course.py
@@ -279,15 +279,15 @@ class CourseTest(CourseTestMixin, TestCase):
         )
         r1 = self.client.get(
             reverse('course-detail-view', args=[self.sandbox_course.pk]))
-        self.assertTrue(r1.context['project_list_grid'])
+        self.assertTrue(r1.context['project_grid_layout'])
 
         r2 = self.client.post(
             reverse('course-detail-view', args=[self.sandbox_course.pk]))
-        self.assertFalse(r2.context['project_list_grid'])
+        self.assertFalse(r2.context['project_grid_layout'])
 
         r3 = self.client.post(
             reverse('course-detail-view', args=[self.sandbox_course.pk]))
-        self.assertTrue(r3.context['project_list_grid'])
+        self.assertTrue(r3.context['project_grid_layout'])
 
 
 class DashboardTest(CourseTestMixin, TestCase):
@@ -344,10 +344,10 @@ class DashboardTest(CourseTestMixin, TestCase):
             )
         )
         r1 = self.client.get(reverse('course-list-view'))
-        self.assertTrue(r1.context['course_list_grid'])
+        self.assertTrue(r1.context['course_grid_layout'])
 
         r2 = self.client.post(reverse('course-list-view'))
-        self.assertFalse(r2.context['course_list_grid'])
+        self.assertFalse(r2.context['course_grid_layout'])
 
         r3 = self.client.post(reverse('course-list-view'))
-        self.assertTrue(r3.context['course_list_grid'])
+        self.assertTrue(r3.context['course_grid_layout'])
