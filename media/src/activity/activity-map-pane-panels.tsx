723e2c53aa003a51b2fde6013eef367f251d5e50
Zarina Mustapha <zarina@columbia.edu>
2021-04-21 13:46:01 -0400

    Theme colors, preliminary implementation

--- media/src/activity/activity-map-pane-panels.tsx
+++ media/src/activity/activity-map-pane-panels.tsx
@@ -117,7 +117,9 @@ export const DefaultPanel: React.FC<DefaultPanelProps> = (
             <ul className='nav nav-tabs pane-content-tabs' style={{ top: paneHeaderHeight }}>
                 {['Overview', 'Base Layers', responseTabTitle].map((el, idx) => {
                     return (
-                        <li className='nav-item button' key={idx}>
+                        <li className={activeTab == idx ?
+                            'nav-item active button' : 'nav-item button'}
+                        key={idx}>
                             <a className={activeTab == idx ?
                                 'nav-link active' : 'nav-link'}
                             href='#'
