a782e6d72050ce2029e1eb932ecec810372966be
Evan Petersen <emp2221@columbia.edu>
2023-08-15 11:52:37 -0400

    docker test

--- locustempus/settings_docker.py
+++ locustempus/settings_docker.py
@@ -1,12 +1,23 @@
 # flake8: noqa
 from locustempus.settings_shared import *
-from ccnmtlsettings.docker import common
 import os
 
-locals().update(
-    common(
-        project=project,
-        base=base,
-        STATIC_ROOT=STATIC_ROOT,
-        INSTALLED_APPS=INSTALLED_APPS,
-    ))
+GDAL_LIBRARY_PATH = '/usr/local/lib'
+
+# docker-compose db container
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.environ.get('POSTGRES_NAME'),
+        'USER': os.environ.get('POSTGRES_USER'),
+        'PASSWORD': os.environ.get('POSTGRES_PASSWORD'),
+        'HOST': 'db',
+        'PORT': 5432,
+    }
+}
+
+
+try:
+    from locustempus.local_settings import *  # noqa: F401,F403
+except ImportError:
+    pass
