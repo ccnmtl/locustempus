2e681a8499b13afd971796707f539df346058544
ndittren <36773036+ndittren@users.noreply.github.com>
2026-03-13 12:03:39 -0400

    LTI 1.3

---
+++ locustempus/lti/urls.py
@@ -0,0 +1,20 @@
+from django.urls import path
+
+from locustempus.lti.views import (
+    JSONConfigView, LtiLaunchView, MyOIDCLoginInitView,
+    DynamicRegistrationView
+)
+from lti_tool.views import jwks
+
+
+urlpatterns = [
+    # django-lti
+    path('.well-known/jwks.json', jwks, name='jwks'),
+    path('init/<uuid:registration_uuid>/',
+         MyOIDCLoginInitView.as_view(), name='oidc_init'),
+    path('<uuid:registration_uuid>/config.json',
+         JSONConfigView.as_view()),
+    path('launch/', LtiLaunchView.as_view(), name='lti-launch'),
+    path('tool_configuration', DynamicRegistrationView.as_view(),
+         name='dynamic_registration'),
+]
